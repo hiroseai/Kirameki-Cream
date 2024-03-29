73f50db4bf3b1e688a2e677bba16a92676367729
ai.hirose <h7tm2a@outlook.jp>
2024-03-29 20:44:52 +0900

    LPコーディング2提出

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,35 +1,56 @@
 /* voice
 ------------------------------------------ */
 var swiper = new Swiper('.swiper', {
-    loop: true,
-    centeredSlides: true,
-    slidesPerView: 1,
-    spaceBetween: 55,
-    breakpoints: {
-        768: {
-            centeredSlides: false,
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-            spaceBetween: 40,
-        },
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  spaceBetween: 55,
+  breakpoints: {
+    768: {
+      centeredSlides: false,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 40,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-  });
-  
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
 /* faq
 ------------------------------------------ */
-  $('.question-text').on('click', function (e) {
-    $(e.target).toggleClass('question-text-active');
+$('.question-text').on('click', function (e) {
+  $(e.target).toggleClass('question-text-active');
+});
+
+$('.question-text').on('click', function (e) {
+  var content = $(e.target).next();
+  content.slideToggle();
+});
+
+/* topbutton
+------------------------------------------ */
+$(function () {
+  var TopBtn = $('.topbutton');
+  TopBtn.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      TopBtn.fadeIn();
+    }
+    else {
+      TopBtn.fadeOut();
+    }
   });
-  
-  $('.question-text').on('click', function (e) {
-    var content = $(e.target).next();
-    content.slideToggle();
-  });
+  TopBtn.click(function() {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 300);
+		return false;
+	});
+});
